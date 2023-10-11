d8f911fea6887bcd91be6f852f149878d035fc50
Arpan Mandal <arpan.rec@gmail.com>
2023-10-11 18:57:35 +0000

    Feature/ip

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"gitlab.com/arpanrecme/initsecureserver/internal/iss"
 	"log"
 	"net/http"
+
+	"gitlab.com/arpanrecme/initsecureserver/internal/iss"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
