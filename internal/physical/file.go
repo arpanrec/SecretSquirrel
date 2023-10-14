3ace4d140e92f8a59e717d5b099487d94100c62c
Arpan Mandal <arpan.rec@gmail.com>
2023-10-15 01:45:24 +0530

    GPG Encryption at rest
    * StorageDataDir
    
    * Utils
    
    * fileserver
    
    * tfstate
    
    * TerraformStateHandler
    
    * rename
    
    * gokeepasslib
    
    * modules
    
    * interface
    
    * tidy
    
    * interface
    
    * sync.Once
    
    * openpgp
    
    * workflows
    
    * concurrency
    
    * concurrency

--- internal/physical/file.go
+++ internal/physical/file.go
@@ -1,52 +1,64 @@
 package physical
 
 import (
+	"github.com/arpanrec/secureserver/internal/common"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
+	"sync"
 )
 
-type FileStorage struct {
-	Location string
-	Data     string
+var mutex = &sync.Mutex{}
+
+var once = &sync.Once{}
+
+type Storage interface {
+	GetData(l string) (string, error)
+	PutData(l string, d string) (bool, error)
+	DeleteData(l string) error
 }
+
+type FileStorage struct{}
+
+var fileStoragePath string
 
 func getPath() string {
-	var StorageDataDir = "data"
-	issDataDirEnv := os.Getenv("ISS_DATA_DIR")
-	if issDataDirEnv != "" {
-		StorageDataDir = issDataDirEnv
-	}
-	return StorageDataDir
+	once.Do(func() {
+		fileStoragePath = common.GetConfig()["storage"].(map[string]interface{})["config"].(map[string]interface{})["path"].(string)
+		log.Printf("File storage path set to %v", fileStoragePath)
+	})
+	return fileStoragePath
 }
 
-func (fs FileStorage) GetData() (string, error) {
-	p := path.Join(getPath(), fs.Location)
-
-	// Read the file and return the contents
+func (fs FileStorage) GetData(Location string) (string, error) {
+	p := path.Join(getPath(), Location)
 	d, err := os.ReadFile(p)
 	return string(d), err
 }
 
-func (fs FileStorage) PutData() (bool, error) {
-	p := path.Join(getPath(), fs.Location)
+func (fs FileStorage) PutData(Location string, Data string) (bool, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	p := path.Join(getPath(), Location)
 	dir := filepath.Dir(p)
 	errMakeDir := os.MkdirAll(dir, 0755)
 	if errMakeDir != nil {
-		log.Fatal(errMakeDir)
+		log.Fatal("Error creating directory: ", errMakeDir)
 		return false, errMakeDir
 	}
-	errWriteFile := os.WriteFile(p, []byte(fs.Data), 0644)
+	errWriteFile := os.WriteFile(p, []byte(Data), 0644)
 	if errWriteFile != nil {
-		log.Fatal(errWriteFile)
+		log.Fatal("Error writing file: ", errWriteFile)
 		return false, errWriteFile
 	}
 	return true, nil
 }
 
-func (fs FileStorage) DeleteData() error {
-	p := path.Join(getPath(), fs.Location)
+func (fs FileStorage) DeleteData(Location string) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+	p := path.Join(getPath(), Location)
 	err := os.Remove(p)
 	return err
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,27 +1,47 @@
 package storage
 
 import (
+	"github.com/arpanrec/secureserver/internal/common"
 	"github.com/arpanrec/secureserver/internal/physical"
+	"log"
+	"sync"
 )
 
-func getStorage(location string, data string) physical.Storage {
-	return physical.FileStorage{
-		Location: location,
-		Data:     data,
+var pStorage physical.Storage
+
+var once = &sync.Once{}
+
+func getStorage() physical.Storage {
+	once.Do(func() {
+		storageType := common.GetConfig()["storage"].(map[string]interface{})["type"].(string)
+		log.Print("Storage type set to ", storageType)
+		switch storageType {
+		case "file":
+			pStorage = physical.FileStorage{}
+		default:
+			log.Panicf("Invalid storage type %v\n", storageType)
+		}
+	})
+	return pStorage
+}
+
+func GetData(l string) (string, error) {
+	s := getStorage()
+	d, err := s.GetData(l)
+	if err != nil {
+		return "", err
 	}
+	decryptMessage(&d)
+	return d, nil
 }
 
-func GetData(location string) (string, error) {
-	s := getStorage(location, "")
-	return s.GetData()
+func PutData(l string, d string) (bool, error) {
+	s := getStorage()
+	encryptMessage(&d)
+	return s.PutData(l, d)
 }
 
-func PutData(location string, data string) (bool, error) {
-	s := getStorage(location, data)
-	return s.PutData()
-}
-
-func DeleteData(location string) error {
-	s := getStorage(location, "")
-	return s.DeleteData()
+func DeleteData(l string) error {
+	s := getStorage()
+	return s.DeleteData(l)
 }
