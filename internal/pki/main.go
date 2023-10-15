7facecd3ae456daee675e51c839dec0102d0d5c7
arpanrec <arpan.rec@gmail.com>
2023-10-16 04:28:06 +0530

    clean

--- internal/pki/main.go
+++ internal/pki/main.go
@@ -1,12 +1,1 @@
 package pki
-
-type ConfigPki struct {
-	CaCertFile                 string `json:"openssl_root_ca_cert_file,omitempty"`
-	CaPrivateKeyFile           string `json:"openssl_root_ca_key_file,omitempty"`
-	CaPrivateKeyPasswordFile   string `json:"openssl_root_ca_key_password_file,omitempty"`
-	CaPrivateKeyNoPasswordFile string `json:"openssl_root_ca_key_NoPassword_file,omitempty"`
-	CaCert                     string `json:"openssl_root_ca_cert,omitempty"`
-	CaPrivateKey               string `json:"openssl_root_ca_key,omitempty"`
-	CaPrivateKeyPassword       string `json:"openssl_root_ca_key_password,omitempty"`
-	CaPrivateKeyNoPassword     string `json:"openssl_root_ca_key_NoPassword,omitempty"`
-}
