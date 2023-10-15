efb72b46c3a2eb407c4aa06d180af34617a21750
arpanrec <arpan.rec@gmail.com>
2023-10-16 00:56:23 +0530

    ip

--- internal/common/main.go
+++ internal/common/main.go
@@ -14,6 +14,26 @@ var config map[string]interface{}
 var mo = &sync.Once{}
 var mu = &sync.Mutex{}
 
+type EncryptionConfig struct {
+	GPGPrivateKeyPath string `json:"private_key_path"`
+	GPGPublicKeyPath  string `json:"public_key_path"`
+	GPGPassphrasePath string `json:"private_key_password_path"`
+	DeleteKeys        bool   `json:"delete_key_files_after_startup"`
+}
+
+type PkiConfig struct {
+	CaCertFile               string `json:"openssl_root_ca_cert_path"`
+	CaPrivateKeyFile         string `json:"openssl_root_ca_key_path"`
+	CaPrivateKeyPasswordFile string `json:"openssl_root_ca_key_password_path"`
+	CaPrivateKeyNopassFile   string `json:"openssl_root_ca_key_nopasswd_path"`
+	DeleteKeys               bool   `json:"delete_key_files_after_startup"`
+}
+
+type MasterConfig struct {
+	Encryption EncryptionConfig `json:"encryption"`
+	PkiConfig  PkiConfig        `json:"pki"`
+}
+
 func GetConfig() map[string]interface{} {
 	mu.Lock()
 	mo.Do(func() {
---
+++ internal/pki/main_test.go
@@ -0,0 +1,44 @@
+package pki
+
+import (
+	"encoding/json"
+	"log"
+	"os/exec"
+	"testing"
+
+	"github.com/arpanrec/secureserver/internal/common"
+)
+
+func TestCertCa(t *testing.T) {
+	// caKeyPath := common.GetConfig()["pki"].(map[string]interface{})["openssl_root_ca_key_path"].(string)
+	// caKeyPasswordPath := common.GetConfig()["pki"].(map[string]interface{})["openssl_root_ca_key_password_path"].(string)
+	// caNopasswdKeyPath := common.GetConfig()["pki"].(map[string]interface{})["openssl_root_ca_key_nopasswd_path"].(string)
+	// log.Printf("Removing password from CA key: %s", caKeyPath)
+	// removePassCmd := exec.Command("openssl",
+	// 	"rsa",
+	// 	"-in", caKeyPath,
+	// 	"-passin", "file:"+caKeyPasswordPath,
+	// 	"-passout", "pass:\"\"",
+	// 	"-out", caNopasswdKeyPath)
+	// if err := removePassCmd.Run(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// t.Log("Removed password from CA key")
+	var pkiJsonSettings ConfigJsonPki
+	pkiJsonSettingsString := common.GetConfig()["pki"].(string)
+	err := json.Unmarshal([]byte(pkiJsonSettingsString), &pkiJsonSettings)
+	if err != nil {
+		log.Fatal("Error unmarshalling pki settings: ", err)
+	}
+	log.Printf("Removing password from CA key: %s", pkiJsonSettings.CaPrivateKeyFile)
+	removePassCmd := exec.Command("openssl",
+		"rsa",
+		"-in", pkiJsonSettings.CaPrivateKeyFile,
+		"-passin", "file:"+pkiJsonSettings.CaPrivateKeyPasswordFile,
+		"-passout", "pass:\"\"",
+		"-out", pkiJsonSettings.CaPrivateKeyNopassFile)
+	if err := removePassCmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	t.Log("Removed password from CA key")
+}
