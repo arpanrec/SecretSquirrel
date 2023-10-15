5f0a3e3062cf2d3a8a40945964ace5168036060c
Arpan Mandal <arpan.rec@gmail.com>
2023-10-16 02:38:03 +0530

    Feature/config (#6)

--- internal/physical/file.go
+++ internal/physical/file.go
@@ -1,7 +1,7 @@
 package physical
 
 import (
-	"github.com/arpanrec/secureserver/internal/common"
+	"github.com/arpanrec/secureserver/internal/serverconfig"
 	"log"
 	"os"
 	"path"
@@ -9,32 +9,40 @@ import (
 	"sync"
 )
 
-type FileStorage struct{}
-
-var fileStoragePath string
-
 var mutexPhysicalFile = &sync.Mutex{}
 
 var oncePhysicalFile = &sync.Once{}
 
-func getPath() string {
-	oncePhysicalFile.Do(func() {
-		fileStoragePath = common.GetConfig()["storage"].(map[string]interface{})["config"].(map[string]interface{})["path"].(string)
-		log.Printf("File storage path set to %v", fileStoragePath)
-	})
-	return fileStoragePath
+type FileStorageConfig struct {
+	Path string `json:"path"`
 }
 
-func (fs FileStorage) GetData(Location string) (string, error) {
-	p := path.Join(getPath(), Location)
+var fileStorageConfigVar FileStorageConfig
+
+func getPath() FileStorageConfig {
+	oncePhysicalFile.Do(func() {
+		storagePath := serverconfig.GetConfig().Storage.Config["path"].(string)
+		if storagePath == "" {
+			log.Fatalln("Fatal Storage path not set")
+		}
+		fileStorageConfigVar = FileStorageConfig{
+			Path: storagePath,
+		}
+		log.Printf("File storage path set to %v", fileStorageConfigVar)
+	})
+	return fileStorageConfigVar
+}
+
+func (fs FileStorageConfig) GetData(Location string) (string, error) {
+	p := path.Join(getPath().Path, Location)
 	d, err := os.ReadFile(p)
 	return string(d), err
 }
 
-func (fs FileStorage) PutData(Location string, Data string) (bool, error) {
+func (fs FileStorageConfig) PutData(Location string, Data string) (bool, error) {
 	mutexPhysicalFile.Lock()
 	defer mutexPhysicalFile.Unlock()
-	p := path.Join(getPath(), Location)
+	p := path.Join(getPath().Path, Location)
 	dir := filepath.Dir(p)
 	errMakeDir := os.MkdirAll(dir, 0755)
 	if errMakeDir != nil {
@@ -49,10 +57,10 @@ func (fs FileStorage) PutData(Location string, Data string) (bool, error) {
 	return true, nil
 }
 
-func (fs FileStorage) DeleteData(Location string) error {
+func (fs FileStorageConfig) DeleteData(Location string) error {
 	mutexPhysicalFile.Lock()
 	defer mutexPhysicalFile.Unlock()
-	p := path.Join(getPath(), Location)
+	p := path.Join(getPath().Path, Location)
 	err := os.Remove(p)
 	return err
 }
---
+++ internal/serverconfig/main.go
@@ -0,0 +1,65 @@
+package serverconfig
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"sync"
+)
+
+var masterServerConfig MasterConfig
+
+var mo = &sync.Once{}
+var mu = &sync.Mutex{}
+
+type EncryptionConfig struct {
+	GPGPrivateKeyPath string `json:"private_key_path"`
+	GPGPublicKeyPath  string `json:"public_key_path"`
+	GPGPassphrasePath string `json:"private_key_password_path"`
+	DeleteKeys        bool   `json:"delete_key_files_after_startup"`
+}
+
+type PkiConfig struct {
+	CaCertFile               string `json:"openssl_root_ca_cert_path"`
+	CaPrivateKeyFile         string `json:"openssl_root_ca_key_path"`
+	CaPrivateKeyPasswordFile string `json:"openssl_root_ca_key_password_path"`
+	CaPrivateKeyNopassFile   string `json:"openssl_root_ca_key_nopasswd_path"`
+	DeleteKeys               bool   `json:"delete_key_files_after_startup"`
+}
+
+type StorageConfig struct {
+	StorageType string                 `json:"type"`
+	Config      map[string]interface{} `json:"config"`
+}
+
+type UserConfig struct {
+}
+
+type MasterConfig struct {
+	Encryption EncryptionConfig      `json:"encryption"`
+	PkiConfig  PkiConfig             `json:"pki"`
+	Storage    StorageConfig         `json:"storage"`
+	UserDb     map[string]UserConfig `json:"users"`
+}
+
+func GetConfig() MasterConfig {
+	mu.Lock()
+	mo.Do(func() {
+		log.Printf("Setting config from %v", "config.json")
+		configFilePath := os.Getenv("SECURE_SERVER_CONFIG_FILE_PATH")
+		if configFilePath == "" {
+			configFilePath = "/home/clouduser/workspace/secureserver/config.json"
+		}
+		configJson, er := os.ReadFile(configFilePath)
+		if er != nil {
+			log.Fatalln("Error reading config file", er)
+		}
+		err := json.Unmarshal(configJson, &masterServerConfig)
+		if err != nil {
+			log.Fatalln("Error unmarshalling config file ", err)
+		}
+		log.Printf("Config set successfully %v\n", masterServerConfig)
+	})
+	mu.Unlock()
+	return masterServerConfig
+}
