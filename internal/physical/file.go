9860f839a1d51dab6dd1623cd56d582af9ffc0a1
arpanrec <me@arpanrec.com>
2023-10-15 22:35:05 +0530

    mutexPhysicalFile

---
+++ internal/physical/file.go
@@ -0,0 +1,58 @@
+package physical
+
+import (
+	"github.com/arpanrec/secureserver/internal/common"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"sync"
+)
+
+type FileStorage struct{}
+
+var fileStoragePath string
+
+var mutexPhysicalFile = &sync.Mutex{}
+
+var oncePhysicalFile = &sync.Once{}
+
+func getPath() string {
+	oncePhysicalFile.Do(func() {
+		fileStoragePath = common.GetConfig()["storage"].(map[string]interface{})["config"].(map[string]interface{})["path"].(string)
+		log.Printf("File storage path set to %v", fileStoragePath)
+	})
+	return fileStoragePath
+}
+
+func (fs FileStorage) GetData(Location string) (string, error) {
+	p := path.Join(getPath(), Location)
+	d, err := os.ReadFile(p)
+	return string(d), err
+}
+
+func (fs FileStorage) PutData(Location string, Data string) (bool, error) {
+	mutexPhysicalFile.Lock()
+	defer mutexPhysicalFile.Unlock()
+	p := path.Join(getPath(), Location)
+	dir := filepath.Dir(p)
+	errMakeDir := os.MkdirAll(dir, 0755)
+	if errMakeDir != nil {
+		log.Println("Error creating directory: ", errMakeDir)
+		return false, errMakeDir
+	}
+	errWriteFile := os.WriteFile(p, []byte(Data), 0644)
+	if errWriteFile != nil {
+		log.Println("Error writing file: ", errWriteFile)
+		return false, errWriteFile
+	}
+	return true, nil
+}
+
+func (fs FileStorage) DeleteData(Location string) error {
+	mutexPhysicalFile.Lock()
+	defer mutexPhysicalFile.Unlock()
+	p := path.Join(getPath(), Location)
+	err := os.Remove(p)
+	return err
+}
--- internal/physical/main.go
+++ internal/physical/main.go
@@ -1,65 +1,7 @@
 package physical
 
-import (
-	"log"
-	"os"
-	"path"
-	"path/filepath"
-	"sync"
-
-	"github.com/arpanrec/secureserver/internal/common"
-)
-
-var mutex = &sync.Mutex{}
-
-var once = &sync.Once{}
-
 type Storage interface {
 	GetData(l string) (string, error)
 	PutData(l string, d string) (bool, error)
 	DeleteData(l string) error
-}
-
-type FileStorage struct{}
-
-var fileStoragePath string
-
-func getPath() string {
-	once.Do(func() {
-		fileStoragePath = common.GetConfig()["storage"].(map[string]interface{})["config"].(map[string]interface{})["path"].(string)
-		log.Printf("File storage path set to %v", fileStoragePath)
-	})
-	return fileStoragePath
-}
-
-func (fs FileStorage) GetData(Location string) (string, error) {
-	p := path.Join(getPath(), Location)
-	d, err := os.ReadFile(p)
-	return string(d), err
-}
-
-func (fs FileStorage) PutData(Location string, Data string) (bool, error) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	p := path.Join(getPath(), Location)
-	dir := filepath.Dir(p)
-	errMakeDir := os.MkdirAll(dir, 0755)
-	if errMakeDir != nil {
-		log.Println("Error creating directory: ", errMakeDir)
-		return false, errMakeDir
-	}
-	errWriteFile := os.WriteFile(p, []byte(Data), 0644)
-	if errWriteFile != nil {
-		log.Println("Error writing file: ", errWriteFile)
-		return false, errWriteFile
-	}
-	return true, nil
-}
-
-func (fs FileStorage) DeleteData(Location string) error {
-	mutex.Lock()
-	defer mutex.Unlock()
-	p := path.Join(getPath(), Location)
-	err := os.Remove(p)
-	return err
 }
