58f42efae06e8663a5de75d213b8da6a0d39bf92
arpanrec <arpan.rec@gmail.com>
2023-10-16 03:57:35 +0530

    ip

--- internal/pki/main_test.go
+++ internal/pki/main_test.go
@@ -10,12 +10,6 @@ import (
 func TestCertCa(t *testing.T) {
 	pkiJsonSettingsString := serverconfig.GetConfig().PkiConfig
 	log.Printf("Removing password from CA key: %s", pkiJsonSettingsString.CaPrivateKeyFile)
-	log.Println("openssl",
-		"rsa",
-		"-in", pkiJsonSettingsString.CaPrivateKeyFile,
-		"-passin", "file:"+pkiJsonSettingsString.CaPrivateKeyPasswordFile,
-		"-passout", "pass:\"\"",
-		"-out", pkiJsonSettingsString.CaPrivateKeyNopassFile)
 	removePassCmd := exec.Command("openssl",
 		"rsa",
 		"-in", pkiJsonSettingsString.CaPrivateKeyFile,
