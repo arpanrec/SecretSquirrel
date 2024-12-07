82cea33f5f3ae60b23e3bd4677356930b4fe9319
arpan <arpan.rec@gmail.com>
2024-12-07 22:15:17 +0530

    Add AES encryption/decryption functionality and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,7 @@
-use std::{
-    io::{BufReader, Read, Write},
-    net::{TcpListener, TcpStream},
-    str,
-    thread::spawn,
-    time::Duration,
-};
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        spawn(
-            || match stream.set_read_timeout(Some(Duration::from_secs(3))) {
-                Ok(_) => {
-                    handle_connection(stream);
-                }
-                Err(e) => {
-                    println!("Error setting read timeout: {:?}", e);
-                }
-            },
-        );
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let mut header_done: bool = false;
-    let mut headers: Vec<String> = Vec::new();
-    let mut body: Vec<u8> = Vec::new();
-    let mut remaining_content_length: i64 = 0;
-    let mut current_line: Vec<u8> = Vec::new();
-
-    for byte in buf_reader.bytes() {
-        let byte = byte.unwrap();
-        println!("Processing byte: {:?}", byte);
-        current_line.push(byte);
-        if !header_done && current_line.ends_with(&[13, 10]) {
-            // Remove last two bytes
-            current_line.pop();
-            current_line.pop();
-            let line = String::from_utf8(current_line.clone()).unwrap();
-            current_line.clear();
-            if !header_done && line == "" {
-                println!("Found empty line");
-                header_done = true;
-            }
-            if !header_done && line.starts_with("Content-Length") {
-                let parts: Vec<&str> = line.split(":").collect();
-                remaining_content_length = parts[1].trim().parse().unwrap();
-            }
-            println!("Found header: {:?}", line);
-            headers.push(line);
-            if header_done && remaining_content_length == 0 {
-                break;
-            }
-        } else if header_done && remaining_content_length > 0 {
-            remaining_content_length -= 1;
-            body.push(byte.clone());
-            if remaining_content_length == 0 {
-                println!("Body done");
-                break;
-            }
-        } else {
-            println!("What is this?");
-        }
-    }
-
-    println!("Headers: {:?}", headers);
-    // println!("Body: {:?}", String::from_utf8(body.clone()).unwrap());
-    println!("Body: {:?}", str::from_utf8(&body).unwrap());
-    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+use axum::{routing::get, Router};
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ tests/aes.rs
@@ -0,0 +1,15 @@
+use secretsquirrel::{aes256_enc, aes256_dec};
+
+#[tokio::test]
+async fn aes_test() {
+    let key_str_base64 = "bCpu2ln1ivhkBlo1iYWfewMdi4yvQHEDnmClTj0ZNPU=";
+    let iv_str_base64 = "+0Vfhn16YpMKYQNOvnP/AA==";
+    let plaintext = "hello world! this is my plaintext.";
+    let encrypted_text = "d618sNKZ9ouOIn4M5IiIanT5T14cJTJMxJ0d9xmo/hRf+TtuHB6G6tIkzq4viTSo";
+    let enc = aes256_enc(key_str_base64, iv_str_base64, plaintext);
+    println!("{:?}", enc);
+    assert_eq!(enc, encrypted_text);
+    let dec = aes256_dec(key_str_base64, iv_str_base64, &enc);
+    println!("{:?}", dec);
+    assert_eq!(dec, plaintext);
+}
