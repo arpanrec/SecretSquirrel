159a09da343323ed09f242bd8e96faf2f75f98fc
arpan <arpan.rec@gmail.com>
2025-02-12 22:33:54 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,48 @@
-use axum::{extract::Path, http::Response, routing::get, Router};
 use axum::http::StatusCode;
+use axum::{extract::Path, http::Response, routing::get, Router};
 use secretsquirrel::physical::{delete, read, write};
 
 #[tokio::main]
 async fn main() {
-    // write("test", "Hello, World!safdasfas1").await;
-    // println!("{:?}", read("test").await);
-    // delete("test").await;
     let app = Router::new().route(
         "/secret/{*key}",
-        get(|Path(path): Path<String>| async move {
-            // Response::builder()
-            //     .status(StatusCode::OK)
-            //     .header("Content-Type", "text/plain")
-            //     .body(read(&path).await.unwrap())
-            //     .unwrap()
-            println!("Received request for key: {}", path);
-            let value = read(&path).await;
-            println!("Read value: {:?}", value);
-            if value.is_some() {
-                let value = value.unwrap();
-                println!("Found value: {}", value);
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Content-Type", "text/plain")
-                    .body(value)
-                    .expect("Failed to send response");
-            } else {
-                println!("Not Found");
-                Response::builder()
-                    .status(404)
-                    .header("Content-Type", "text/plain")
-                    .body("")
-                    .expect("Failed to send response");
-            };
-        }),
+        get(handle_get).post(handle_post).delete(handle_delete),
     );
-    // let app: Router = Router::new().route("/{*key}", get(handler));
-    //
-    // async fn handler(Path(path): Path<String>) -> String {
-    //     path
-    // }
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_get(Path(path): Path<String>) -> Response<String> {
+    println!("Received request for key: {}", path);
+    let value = read(&path).await;
+    println!("Read value: {:?}", value);
+    let builder = Response::builder();
+    if let Some(value) = value {
+        println!("Found value: {}", value);
+        builder
+            .status(StatusCode::OK)
+            .header("Content-Type", "text/plain")
+            .body(value)
+            .expect("Failed to send response")
+    } else {
+        println!("Not Found");
+        builder
+            .status(StatusCode::NOT_FOUND)
+            .header("Content-Type", "text/plain")
+            .body("".to_string())
+            .expect("Failed to send response")
+    }
+}
+
+async fn handle_post(Path(path): Path<String>, body: String) -> Response<String> {
+    println!("Received request for key: {}", path);
+    println!("Received body: {}", body);
+    write(&path, &body).await;
+    Response::new("".to_string())
+}
+
+async fn handle_delete(Path(path): Path<String>) -> Response<String> {
+    println!("Received request for key: {}", path);
+    delete(&path).await;
+    Response::new("".to_string())
 }
