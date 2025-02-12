b6a9a781bcd2b8ebbb923a351485c9299d573331
arpan <arpan.rec@gmail.com>
2025-02-12 23:29:49 +0530

    Router working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use axum::http::StatusCode;
-use axum::{extract::Path, http::Response, routing::get, Router};
+use axum::{
+    body::Body,
+    extract::Path,
+    http::{Request, Response, StatusCode},
+    routing::get,
+    Router,
+};
 use secretsquirrel::physical::{delete, read, write};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/secret/{*key}",
-        get(handle_get).post(handle_post).delete(handle_delete),
-    );
+    let app = Router::new()
+        .route(
+            "/secret/{*key}",
+            get(handle_get).post(handle_post).delete(handle_delete),
+        )
+        .route("/{*key}", axum::routing::any(handle_any));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
@@ -44,5 +51,15 @@ async fn handle_post(Path(path): Path<String>, body: String) -> Response<String>
 async fn handle_delete(Path(path): Path<String>) -> Response<String> {
     println!("Received request for key: {}", path);
     delete(&path).await;
+    Response::new("".to_string())
+}
+
+// pub fn any<H, T, S>(handler: H) -> MethodRouter<S, Infallible>
+async fn handle_any(Path(path): Path<String>, request: Request<Body>) -> Response<String> {
+    println!(
+        "Received request for lock with method: {:?}",
+        request.method()
+    );
+    println!("Received request for key: {}", path);
     Response::new("".to_string())
 }
