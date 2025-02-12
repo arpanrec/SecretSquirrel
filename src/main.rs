1969e6986435d91e42dfb4e15dc369c5e06cc125
arpan <arpan.rec@gmail.com>
2025-02-12 20:37:51 +0530

    Refactor database connection handling in physical module and simplify function signatures

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use axum::{routing::get, Router};
-use secretsquirrel::physical::{delete, establish_connection, read, write};
+use secretsquirrel::physical::{delete, read, write};
 
 #[tokio::main]
 async fn main() {
-    let conn = establish_connection()
-        .await
-        .expect("Failed to connect to database");
-    write(&conn, "test", "Hello, World!safdasfas1").await;
-    println!("{:?}", read(&conn, "test").await);
-    delete(&conn, "test").await;
+    write("test", "Hello, World!safdasfas1").await;
+    println!("{:?}", read("test").await);
+    delete("test").await;
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     // axum::serve(listener, app).await.unwrap();
--- src/physical.rs
+++ src/physical.rs
@@ -12,12 +12,12 @@ use std::time::{SystemTime, UNIX_EPOCH};
 // );
 // CREATE UNIQUE INDEX secrets_a3_key_a3_IDX ON secrets_a3 (key_a3,version_a3);
 
-pub async fn establish_connection() -> libsql::Result<Connection> {
+async fn establish_connection() -> libsql::Connection {
     let url = std::env::var("TURSO_DATABASE_URL").expect("TURSO_DATABASE_URL must be set");
     let token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set");
 
-    let db = Builder::new_remote(url, token).build().await?;
-    db.connect()
+    let db = Builder::new_remote(url, token).build().await.unwrap();
+    db.connect().unwrap()
 }
 
 async fn get_current_version(conn: &Connection, key: &str) -> i64 {
@@ -35,8 +35,8 @@ async fn get_current_version(conn: &Connection, key: &str) -> i64 {
     }
 }
 
-pub async fn read(conn: &Connection, key: &str) -> Option<String> {
-    let mut rows = conn
+pub async fn read(key: &str) -> Option<String> {
+    let mut rows = establish_connection().await
         .query(
             "SELECT value_a3 FROM secrets_a3 WHERE key_a3 = ? AND is_deleted_a3 = 0 ORDER BY version_a3 DESC LIMIT 1;",
             libsql::params![key],
@@ -50,8 +50,9 @@ pub async fn read(conn: &Connection, key: &str) -> Option<String> {
     }
 }
 
-pub async fn write(conn: &Connection, key: &str, value: &str) -> () {
-    let next_version = get_current_version(conn, key).await + 1;
+pub async fn write(key: &str, value: &str) -> () {
+    let conn = establish_connection().await;
+    let next_version = get_current_version(&conn, key).await + 1;
     let current_epoch_time: i64 = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
@@ -64,7 +65,8 @@ pub async fn write(conn: &Connection, key: &str, value: &str) -> () {
     .unwrap();
 }
 
-pub async fn delete(conn: &Connection, key: &str) -> () {
+pub async fn delete(key: &str) -> () {
+    let conn = establish_connection().await;
     let current_epoch_time: i64 = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
